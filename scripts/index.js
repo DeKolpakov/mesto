153dcf53139c444c9ba4c383aaa0b192b0e1d39a
Deni Kolpakov <kolpakovdeni@yandex.ru>
2023-07-15 15:42:35 +0300

    mod: внесены изменения в JS, добалены стили avatar

--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,59 @@
-// Объявляем переменные
+// Открытие/закрытие формы____________________________________________
+
+// Объявляем переменные и находим элементы
 const popup = document.querySelector(".popup");
-const popupButtonOpen = document.querySelector(".profile__open-popup");
+const popupButtonOpen = document.querySelector(".profile__button-open");
 const popupButtonClose = popup.querySelector(".popup__button-close");
 
-// Создаем функцию для добавления класса
-function popupToggle() {
+// Создаем функцию для добавления/удаления класса
+function popupToggleOpened() {
   popup.classList.toggle("popup_opened");
 }
 
-// Устанавливаем слушатель клика с применением функции
-popupButtonOpen.addEventListener("click", popupToggle);
-popupButtonClose.addEventListener("click", popupToggle);
+// Устанавливаем слушатель клика по кнопке с применением функции
+popupButtonOpen.addEventListener("click", popupToggleOpened);
+popupButtonClose.addEventListener("click", popupToggleOpened);
 
-// Создаем функцию сравнения таргетов для закрытия попапа
+// Создаем функцию сравнения таргетов для закрытия попапа по нажатию на оверлей
 function popupCloseByOverlay(event) {
   if (event.target == event.currentTarget) {
-    popupToggle();
+    popupToggleOpened();
   }
 }
 
 // Устанавливаем слушатель клика с применением функции
 popup.addEventListener("click", popupCloseByOverlay);
 
+// Заполнение формы_____________________________________________________
+
 // Находим форму в DOM
 const formElement = document.querySelector(".popup__container");
 
-// Находим поля формы в DOM
-const nameInput = formElement.querySelector(".popup__input-name");
-const descriptionInput = formElement.querySelector(".popup__input-description");
+// Находим поля формы для ввода текста
+const nameInputPopup = formElement.querySelector(".popup__input-name");
+const descriptionInputPopup = formElement.querySelector(".popup__input-description");
 
-// Находим поля для ввода информации
-const profileName = document.querySelector(".profile__name");
-const profileDescription = document.querySelector(".profile__description");
+// Находим поля профиля для вывода текста
+const nameOutputProfile = document.querySelector(".profile__name");
+const descriptionOutputProfile = document.querySelector(".profile__description");
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
-function handleFormSubmit(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function handleFormSubmit(event) {
+  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   
-  // Получите значение полей jobInput и nameInput из свойства value
-  const nameValue = nameInput.value;
-  const descriptionValue = descriptionInput.value;
+  // Получите значение полей jobInput и nameInputPopup из свойства value
+  const nameValue = nameInputPopup.value;
+  const descriptionValue = descriptionInputPopup.value;
 
   // Вставьте новые значения с помощью textContent
-  profileDescription.textContent = descriptionValue;
-  profileName.textContent = nameValue;
+  nameOutputProfile.textContent = nameValue;
+  descriptionOutputProfile.textContent = descriptionValue;
 
   // Закрываем форму
-  popupToggle();
+  popupToggleOpened();
 
   // Очищаем форму
-  evt.target.reset();
+  event.target.reset();
 }
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
