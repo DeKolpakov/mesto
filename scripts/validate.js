3c4b174d941d492fd468edc747a146105bc7a1c4
Денис Колпаков <deniskolpakov@MacBook-Pro-Deni.local>
2023-08-15 13:41:09 +0700

    fut: валидация с объектом

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,80 @@
+//______________________функция-включения-валидации______________________________
 
+const enableValidation = ({formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, activeButtonClass, inputErrorClass, errorClass}) => {
+  const formsAll = Array.from(document.querySelectorAll(formSelector));
+  formsAll.forEach((formElement) => {
+    formElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+    setEventListeners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, activeButtonClass, inputErrorClass, errorClass);
+  });
+  //console.log();
+};
+
+//______________________добавление-ошибки____________________
+
+const validationInput = (formElement, inputElement, inputErrorClass, errorClass) => {
+  const inputError = formElement.querySelector(`#${inputElement.id}_error`);
+  if (inputElement.validity.valid) {
+    inputElement.classList.remove(inputErrorClass);
+    inputError.classList.remove(errorClass);
+    inputError.textContent = "";
+  } else {
+    inputElement.classList.add(inputErrorClass);
+    inputError.classList.add(errorClass);
+    inputError.textContent = inputElement.validationMessage;
+  }
+  //console.log();
+};
+
+//_____________________валидация-кнопки__________________________________
+
+const submitButtonState = (inputsAll, submitButton, inactiveButtonClass, activeButtonClass) => {
+  const invalidInputs = inputsAll.some((inputElement) => !inputElement.validity.valid);
+  //console.log();
+  if (invalidInputs) {
+    submitButton.setAttribute("disabled", true);
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.classList.remove(activeButtonClass);
+  } else {
+    submitButton.removeAttribute("disabled", false);
+    submitButton.classList.add(activeButtonClass);
+    submitButton.classList.remove(inactiveButtonClass);
+  }
+};
+
+//_____________________слушатели_________________________________________
+
+const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, activeButtonClass, inputErrorClass, errorClass) => {
+  const inputsAll = Array.from(formElement.querySelectorAll(inputSelector));
+  const submitButton = formElement.querySelector(submitButtonSelector);
+
+  submitButtonState(inputsAll, submitButton, inactiveButtonClass, activeButtonClass);
+
+  inputsAll.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      validationInput(formElement, inputElement, inputErrorClass, errorClass);
+      submitButtonState(inputsAll, submitButton, inactiveButtonClass, activeButtonClass);
+    });
+  });
+  //console.log();
+};
+
+//_________________________________________________________________________
+
+//______________________запуск-валидации______________________________
+
+enableValidation({
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button-save",
+  inactiveButtonClass: "popup__button-save_invalid",
+  activeButtonClass: "popup__button-save_valid",
+  inputErrorClass: "popup__input_error",
+  errorClass: "popup__input-span_error",
+});
+
+/* 
 //____________________валидация по вебинару________________________________
 
 const validateInput = (inputElement) => {
@@ -54,3 +130,4 @@ profileForm.addEventListener("submit", validateForm);
 addImageForm.addEventListener("submit", validateForm);
 profileForm.addEventListener("input", validateFormInput);
 addImageForm.addEventListener("input", validateFormInput);
+ */
